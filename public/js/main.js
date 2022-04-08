a27ddf9a667109ad5eff7594c2ba81b3f902ccf1
Raman <bandariraman@gmail.com>
2022-04-08 19:29:57 +0530

    nav libk

--- public/js/main.js
+++ public/js/main.js
@@ -100,18 +100,5 @@
      * Clients Slider
      */
 
-    // Navbar on mobile
-    let elements = document.querySelectorAll(".nav-link:not(.dropdown-toggle)");
-
-    for (let i = 0; i < elements.length; i++) {
-        elements[i].addEventListener("click", () => {
-            document
-                .querySelector(".offcanvas-collapse")
-                .classList.toggle("open");
-        });
-    }
-
-    document.querySelector(".navbar-toggler").addEventListener("click", () => {
-        document.querySelector(".offcanvas-collapse").classList.toggle("open");
-    });
+   
 })();
