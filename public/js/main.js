49a95552561303ea7e06f721081896fc093ec446
Raman <bandariraman@gmail.com>
2022-04-08 19:42:39 +0530

    nav libk

--- public/js/main.js
+++ public/js/main.js
@@ -1,113 +1,98 @@
 /**
- * Template Name: Green - v4.7.0
- * Template URL: https://bootstrapmade.com/green-free-one-page-bootstrap-template/
- * Author: BootstrapMade.com
- * License: https://bootstrapmade.com/license/
- */
-(function () {
-    "use strict";
+* Template Name: Green - v4.7.0
+* Template URL: https://bootstrapmade.com/green-free-one-page-bootstrap-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+(function() {
+  "use strict";
 
-    /**
-     * Easy selector helper function
-     */
-    const select = (el, all = false) => {
-        el = el.trim();
-        if (all) {
-            return [...document.querySelectorAll(el)];
-        } else {
-            // return document.querySelector(el)
-        }
-    };
-
-    /**
-     * Easy event listener function
-     */
-    const on = (type, el, listener, all = false) => {
-        let selectEl = select(el, all);
-        if (selectEl) {
-            if (all) {
-                selectEl.forEach((e) => e.addEventListener(type, listener));
-            } else {
-                selectEl.addEventListener(type, listener);
-            }
-        }
-    };
-
-    /**
-     * Easy on scroll event listener
-     */
-    const onscroll = (el, listener) => {
-        el.addEventListener("scroll", listener);
-    };
-
-    /**
-     * Navbar links active state on scroll
-     */
-    let navbarlinks = select("#navbar .scrollto", true);
-    const navbarlinksActive = () => {
-        let position = window.scrollY + 200;
-        navbarlinks.forEach((navbarlink) => {
-            if (!navbarlink.hash) return;
-            let section = select(navbarlink.hash);
-            if (!section) return;
-            if (
-                position >= section.offsetTop &&
-                position <= section.offsetTop + section.offsetHeight
-            ) {
-                navbarlink.classList.add("active");
-            } else {
-                navbarlink.classList.remove("active");
-            }
-        });
-    };
-    window.addEventListener("load", navbarlinksActive);
-    onscroll(document, navbarlinksActive);
-
-    /**
-     * Scrolls to an element with header offset
-     */
-
-    /**
-     * Scroll with ofset on page load with hash links in the url
-     */
-    window.addEventListener("load", () => {
-        if (window.location.hash) {
-            if (select(window.location.hash)) {
-                scrollto(window.location.hash);
-            }
-        }
-    });
-
-    /**
-     * Hero carousel indicators
-     */
-    let heroCarouselIndicators = select("#hero-carousel-indicators");
-    let heroCarouselItems = select("#heroCarousel .carousel-item", true);
-
-    heroCarouselItems.forEach((item, index) => {
-        index === 0
-            ? (heroCarouselIndicators.innerHTML +=
-                  "<li data-bs-target='#heroCarousel' data-bs-slide-to='" +
-                  index +
-                  "' class='active'></li>")
-            : (heroCarouselIndicators.innerHTML +=
-                  "<li data-bs-target='#heroCarousel' data-bs-slide-to='" +
-                  index +
-                  "'></li>");
-    });
-
-    /**
-     * Clients Slider
-     */
-
-   
-})();
-
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      // return document.querySelector(el)
+    }
   }
-}
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
+
+  /**
+   * Scrolls to an element with header offset
+   */
+
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash)
+      }
+    }
+  });
+
+  /**
+   * Hero carousel indicators
+   */
+  let heroCarouselIndicators = select("#hero-carousel-indicators")
+  let heroCarouselItems = select('#heroCarousel .carousel-item', true)
+
+  heroCarouselItems.forEach((item, index) => {
+    (index === 0) ?
+    heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "' class='active'></li>":
+      heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "'></li>"
+  });
+
+  /**
+   * Clients Slider
+   */
+  
+  
+
+
+})()
--- resources/js/components/Layouts/Header.js
+++ resources/js/components/Layouts/Header.js
@@ -15,28 +15,6 @@ const Header = (props) => {
     };
     return (
         <React.Fragment>
-            
-<div className="mobile-container">
-
-<div className="topnav">
-  <a href="#home" className="active">Logo</a>
-  <div id="myLinks">
-    <a href="#news">News</a>
-    <a href="#contact">Contact</a>
-    <a href="#about">About</a>
-  </div>
-  <a href="javascript:void(0);" className="icon" onclick="myFunction()">
-    <i className="fa fa-bars"></i>
-  </a>
-</div>
-
-<div style="padding-left:16px">
-  <h3>Vertical Mobile Navbar</h3>
-  <p>This example demonstrates how a navigation menu on a mobile/smart phone could look like.</p>
-  <p>Click on the hamburger menu (three bars) in the top right corner, to toggle the menu.</p>
-</div>
-
-</div>
             <nav
                 id="navbarExample"
                 className="navbar navbar-expand-lg fixed-top navbar-light"
