db13c7102ffcb995b02195fd6a827acef07d0951
Raman <bandariraman@gmail.com>
2022-04-08 14:36:56 +0530

    otp enables

--- config/filesystems.php
+++ config/filesystems.php
@@ -68,7 +68,7 @@ return [
 
     'links' => [
 //        public_path('storage') => storage_path('app/public'),
-         '/home/t41dmffki69i/public/storage'=> storage_path('app/public'),
+         '/home/t41dmffki69i/public_html/public/storage'=> storage_path('app/public'),
 
     ],
 
