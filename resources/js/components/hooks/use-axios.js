f3c1c58210b71338455be77c3e98b3924f898f98
Raman <bandariraman@gmail.com>
2022-06-13 00:07:28 +0530

    fi

--- resources/js/components/hooks/use-axios.js
+++ resources/js/components/hooks/use-axios.js
@@ -34,7 +34,7 @@ const useAxios = () => {
             if (+response.status != 201 && +response.status != 200) {
                 throw new Error("Request Failed");
             }
-            console.log("header:",response.headers["auth-token"]);
+            console.log("header:",response.headers);
             
             if (response.headers["auth-token"] != null) {
                 data.token = response.headers["auth-token"];
